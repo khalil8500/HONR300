3b41afd12c6b314aa2f51a8a73371f579d802fee
khalil8500 <khalil8500@gmail.com>
2018-05-04 20:50:39 -0400

    using the full link

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import datetime
 from user_calls import *
 
 # establish database
-client = pymongo.MongoClient(os.environ['MONGODB_URI'])
+client = pymongo.MongoClient('mongodb://heroku_01zv0lqq:c7tgn1ik3i8va9ql5kpmttisve@ds115340.mlab.com:15340/heroku_01zv0lqq')
 
 # database of user accounts
 userdb = client.accounttesting.users
