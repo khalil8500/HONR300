33d1d8f1c927d2fd6233c9d59f9c9e3c584325c7
khalil8500 <khalil8500@gmail.com>
2018-05-10 04:44:09 -0400

    ugh

--- post_calls.py
+++ post_calls.py
@@ -6,26 +6,28 @@ from datetime import datetime
 @app.route("/post", methods=["POST"])
 @app.route("/dashboard", methods=["POST"])
 def post_attempt():
-    form = dict(request.form)
-    
-    if "text" in form and "anonymous" in form:
-        text = form["text"][0]
-        anonymous = form["anonymous"][0]
+	form = dict(request.form)
+
+	'''
+	if "text" in form and "anonymous" in form:
+		text = form["text"][0]
+		anonymous = form["anonymous"][0]
 
         if (anonymous == "True" or anonymous == "False") and text != "":
-            for c in text:
-                if ord(c) > 127:
-                    return 400
+			for c in text:
+				if ord(c) > 127:
+					return 400
 
-            postdb.insert_one({
-                "user_id": session["user_id"],
-                "text": text,
-                "date_posted": datetime.now(),
-                "anonymous": anonymous
-            })
-            return 200
+			postdb.insert_one({
+				"user_id": session["user_id"],
+				"text": text,
+				"date_posted": datetime.now(),
+				"anonymous": anonymous
+			})
+			return 200
+	'''
 	
-	return 400
+	return 200
 
 
 @app.route("/posts", methods=["GET"])
