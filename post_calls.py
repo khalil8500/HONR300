7482dcce255416a777ccbfee82fca1a1f0fa145c
khalil8500 <khalil8500@gmail.com>
2018-05-10 04:22:13 -0400

    forgot to change the import

--- post_calls.py
+++ post_calls.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, session, json, redirect, url_for, Response
-from app import app, userdb, postdb
+from __init__ import app, userdb, postdb
 from datetime import datetime
 
 
